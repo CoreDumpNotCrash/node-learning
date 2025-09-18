e6c22ec64cb9664ec2e917d2cbaf9e5638b59d90
CoreDumpNotCrash <nazarkolepkov008@gmail.com>
2025-09-18 18:12:13 +0300

    ft: added routing including 404

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res)=>{
-    res.writeHead(200, {"content-type": "text/plain"});
-    res.end("Hello World!");
+    if (req.method === "GET" && req.url === "/") {
+        res.writeHead(200, {"content-type": "text/plain"});
+        res.end("Hello World!");
+    } else if (req.method === "GET" && req.url === "/404") {
+        res.writeHead(404, {"content-type": "text/plain"});
+        res.end("404");
+    } else {
+        res.writeHead(302, {"Location": "/404"});
+        res.end();
+    }
 });
 
 server.listen(3000, 'localhost', ()=>{
