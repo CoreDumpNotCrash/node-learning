17078ba9a6daa65600661d2cec9337769538a8bc
CoreDumpNotCrash <nazarkolepkov008@gmail.com>
2025-09-20 14:51:03 +0300

    refactor: replaced clasic http with expressJS

--- src/index.js
+++ src/index.js
@@ -1,89 +1,68 @@
-const http = require("http");
+const express = require("express");
+const path = require("path");
 const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-    if (req.method === "GET" && req.url === "/") {
-        res.writeHead(200, { "content-type": "text/plain" });
-        res.end("Home page");
-    } else if (req.method === "GET" && req.url === "/api") {
-        fs.readFile(__dirname + "/data/data.json", "utf-8", (err, data) => {
-            if (err) {
-                res.writeHead(500, { "content-type": "text/plain" });
-                res.end("Cannot load data");
-                return;
-            }
+const app = express();
+const PORT = 3000;
+const DATA_PATH = path.join(__dirname, "data/data.json");
 
-            res.writeHead(200, { "content-type": "application/json" });
-            res.end(data);
-        });
-    } else if (req.method === "POST" && req.url === "/api") {
-        let body = "";
-        req.on("data", (chunk) => {
-            body += chunk;
-        });
+app.use(express.json());
 
-        req.on("end", () => {
+app.get("/", (req, res) => {
+    res.end("Home Page");
+});
+
+app.get("/api", (req, res) => {
+    res.sendFile(DATA_PATH, (err) => {
+        if (err) {
+            res.status(500).json({
+                status: "error",
+                message: "Cannot load file",
+            });
+        }
+    });
+});
+
+app.post("/api/:scope", (req, res) => {
+    HandlePost(req, res);
+});
+
+app.post("/api", (req, res) => {
+    HandlePost(req, res);
+});
+
+function HandlePost(req, res) {
+    const scope = req.params.scope;
+    const newData = req.body;
+
+    fs.readFile(DATA_PATH, "utf-8", (err, data) => {
+        let fileData = {};
+        if (!err) {
             try {
-                let new_data = JSON.parse(body);
-                const data_path = __dirname + "/data/data.json";
-
-                fs.readFile(data_path, "utf-8", (err, data) => {
-                    let file_data = [];
-
-                    if (!err && data) {
-                        try {
-                            file_data = JSON.parse(data);
-                            if (!Array.isArray(file_data)) {
-                                file_data = [];
-                            }
-                        } catch (e) {
-                            file_data = [];
-                        }
-                    }
-
-                    file_data.push(new_data);
-
-                    fs.writeFile(
-                        data_path,
-                        JSON.stringify(file_data, null, 2),
-                        (err) => {
-                            if (err) {
-                                res.writeHead(500, {
-                                    "content-type": "application/json",
-                                });
-                                res.end(
-                                    JSON.stringify({
-                                        status: "error",
-                                        message: "problems while saving file",
-                                    })
-                                );
-                                return;
-                            }
-                            res.writeHead(200, {
-                                "content-type": "application/json",
-                            });
-                            res.end(
-                                JSON.stringify({
-                                    status: "ok",
-                                    received: new_data,
-                                })
-                            );
-                        }
-                    );
+                fileData = JSON.parse(data) || {};
+            } catch {}
+        }
+        if (scope) {
+            if (!Array.isArray(fileData[scope])) fileData[scope] = [];
+            fileData[scope].push(newData);
+        } else {
+            if (!Array.isArray(fileData)) fileData = [];
+            fileData.push(newData);
+        }
+        fs.writeFile(DATA_PATH, JSON.stringify(fileData, null, 2), (err) => {
+            if (err)
+                return res.status(500).json({
+                    status: "error",
+                    scope: scope || "root",
+                    message: "Couldnt save data into file",
                 });
-            } catch (e) {
-                res.writeHead(400, { "content-type": "application/json" });
-                res.end(
-                    JSON.stringify({ status: "error", message: "invalid JSON" })
-                );
-            }
+            res.json({
+                status: "ok",
+                scope: scope || "root",
+                received: newData,
+            });
         });
-    } else {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.end("404");
-    }
-});
+    });
+}   
 
-server.listen(3000, "localhost", () => {
-    console.log("Started server at 3000 port");
-});
+app.listen(PORT, () => console.log(`Server launch on localhost:${PORT}`));
